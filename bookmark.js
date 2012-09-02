46a2926c10acd4ae7fbdb17abd0f747fffeb31db
sebastian <sebastian@lisa.(none)>
2012-09-02 19:51:19 +0200

    Added missing } to bookmark.js min

--- bookmark.js
+++ bookmark.js
@@ -4,4 +4,4 @@ javascript: (function () {
     s.src = 'https://raw.github.com/kattfan/swedbank-export/master/swedbank.js';
     document.body.appendChild(s);
 })();
-//MIN:javascript: (function () {s = document.createElement('script');s.type = 'text/javascript';s.src = 'https://raw.github.com/kattfan/swedbank-export/master/swedbank.js';document.body.appendChild(s);)();
+//Min: javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://raw.github.com/kattfan/swedbank-export/master/swedbank.js';document.body.appendChild(s);})();
