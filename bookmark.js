039e50abf2fa4e97955cff45f88404c94ef792d0
Ludvig Ericson <ludvig@lericson.se>
2013-02-15 23:49:46 +0100

    Set new Github username

--- bookmark.js
+++ bookmark.js
@@ -1,7 +1,7 @@
 javascript: (function () {
     s = document.createElement('script');
     s.type = 'text/javascript';
-    s.src = 'https://raw.github.com/kattfan/swedbank-export/master/swedbank.js';
+    s.src = 'https://raw.github.com/lericson/swedbank-export/master/swedbank.js';
     document.body.appendChild(s);
 })();
-//Min: javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://raw.github.com/kattfan/swedbank-export/master/swedbank.js';document.body.appendChild(s);})();
+//Min: javascript:(function(){s=document.createElement('script');s.type='text/javascript';s.src='https://raw.github.com/lericson/swedbank-export/master/swedbank.js';document.body.appendChild(s);})();
